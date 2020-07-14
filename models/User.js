9ceb28472768a6c7e66cb2f45f19ef3921d62319
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-07-14 14:14:42 -0500

    update User model

--- models/User.js
+++ models/User.js
@@ -3,20 +3,44 @@ const { Schema, model } = require('mongoose');
 const UserSchema = new Schema(
     {
         username: {
-            type: String
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
         },
         email: {
             type: String,
+            required: true,
+            unique: true
         },
         // add thoughts
-        thoughts: [],
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
         // add friends
-        friends: []
+        friends: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'Friend'
+            }
+        ]
     },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
 );
 
 const User = model('User', UserSchema);
 
-// add virtual - friendCount
+// add virtual to get total count of user's friends
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
 
 module.exports = User;
